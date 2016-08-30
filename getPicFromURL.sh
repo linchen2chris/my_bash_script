6c9ef65cc13292d07cd7f63a0db81c63660ea1ca
chris <lc1990linux@gmail.com>
2016-08-30 15:58:42 +0800

    it can get Pic from the URL

---
+++ getPicFromURL.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+if [ $# -ne 3 ];
+then
+    echo "Usage $0 URL -d DIRECTORY"
+    exit -1
+fi
+
+for i in {1..4}
+do
+    case $1 in
+        -d) shift; directory=$1; shift;;
+        *) url=${url:-$1}; shift;;
+    esac
+done
+
+echo "mkdir the $directory"
+mkdir -p $directory;
+baseurl=$(echo $url | egrep -o "https?://[a-z.]+")
+
+echo Downloading $url
+curl -s $url |egrep -o "<img src=[^>]*>" |
+    sed 's/<img src=\"\([^"]*\).*/\1/g' > ~/$$.list
+
+sed -i "s|^/|$baseurl/|" ~/$$.list
+
+cd $directory;
+
+while read filename;
+do
+    echo Downloading $filename
+    curl -s -O "$filename" 
+done < ~/$$.list
